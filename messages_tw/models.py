aca52dfa9a51ef3089c5ff60e10552ef06272d88
caiovictormc <caiovictormc@gmail.com>
2020-03-23 19:17:10 -0300

    messages_tw: Save messages

--- messages_tw/models.py
+++ messages_tw/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class MessageManager(models.Manager):
+    def repeatedly_recently(self, message: "Message"):
+        queryset = self.get_queryset()
+        return queryset.filter(
+            number_from=message.number_from,
+            number_to=message.number_to,
+            content=message.content,
+            kind=message.kind,
+            blocked=False,
+            has_error=False,
+        )
+
+
+class Message(models.Model):
+    WHATSAPP = "whatsapp"
+    SMS = "sms"
+
+    KIND_CHOICES = (
+        (WHATSAPP, "Whatsapp"),
+        (SMS, "SMS"),
+    )
+    number_from = models.CharField(max_length=75)
+    number_to = models.CharField(max_length=75)
+    content = models.TextField()
+
+    kind = models.CharField(choices=KIND_CHOICES, max_length=15)
+    has_blocked = models.BooleanField(default=False)
+    has_error = models.BooleanField(default=False)
+
+    creator = models.ForeignKey(
+        User, null=True, on_delete=models.SET_NULL, related_name="messages"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    objects = MessageManager()
+
+    def __str__(self):
+        return f"{self.pk}: {self.number_from} - {self.number_to}"
--- messages_tw/views.py
+++ messages_tw/views.py
@@ -5,8 +5,11 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response as DRFResponse
 from rest_framework import status
+from twilio.base.exceptions import TwilioException
 
 from twilio.rest import Client
+
+from messages_tw.models import Message
 
 
 class BaseMessageSender(APIView):
@@ -17,45 +20,77 @@ class BaseMessageSender(APIView):
     TOKEN_TW = settings.AUTH_TOKEN
     FROM = settings.FROM_
 
-    #API Twilio connection
-    def client_message(self, to, from_, message):
-        client = Client(self.ACCOUNT_SID, self.TOKEN_TW)
-        message = client.messages.create(
-            to=to,
-            from_=from_,
-            body=message,
-        )
+    def _client_message(self, message: Message):
+        try:
+            client = Client(self.ACCOUNT_SID, self.TOKEN_TW)
+            client.messages.create(
+                to=message.number_to,
+                from_=message.number_from,
+                body=message.content,
+            )
+        except TwilioException as e:
+            message.has_error = True
+            message.save()
+            raise e
 
-    def send_whatsapp(self, to, message):
-        to_whatsapp = 'whatsapp:'+to
-        from_ = 'whatsapp:'+ self.FROM
+    def _send_message(self, to: str, message_text: str, kind: str):
+        if kind == Message.WHATSAPP:
+            message = Message(
+                creator=self.request.user,
+                number_from=f"whatsapp:{self.FROM}",
+                number_to=f"whatsapp:{to}",
+                content=message_text,
+                kind=Message.WHATSAPP,
+            )
+        else:
+            message = Message.objects.create(
+                creator=self.request.user,
+                number_from=self.FROM,
+                number_to=to,
+                content=message_text,
+                kind=Message.SMS,
+            )
 
-        self.client_message(to_whatsapp, from_, message)
+        repeatedly_qs = Message.objects.repeatedly_recently(message)
+        if repeatedly_qs.exists():
+            message.has_blocked = True
+            message.save()
+        else:
+            self._client_message(message)
 
-    def send_sms(self, to, message):
-        to_sms = to
-        from_ = self.FROM
+        return message
 
-        self.client_message(to_sms, from_, message)
+    def _handle_response(self, message: Message):
+        if message.has_blocked:
+            data = {"error": "A similar message has been sent recently"}
+            status_code = status.HTTP_400_BAD_REQUEST
+        elif message.has_error:
+            data = {"error": "Twilio integration error"}
+            status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        else:
+            data = {"message": "Sent successfully"}
+            status_code = status.HTTP_200_OK
+
+        return data, status_code
 
 
 class SendWhatsappMessage(BaseMessageSender):
-
     def post(self, request, *args, **kwargs):
         to = request.data["to"]
         message = request.data["message"]
 
-        self.send_whatsapp(to=to, message=message)
+        message = self._send_message(to, message, Message.WHATSAPP)
+        data, status_code = self._handle_response(message)
 
-        return DRFResponse(request.data, status=status.HTTP_200_OK)
+        return DRFResponse(data, status=status_code)
 
 
 class SendSMSMessage(BaseMessageSender):
-
     def post(self, request, *args, **kwargs):
         to = request.data["to"]
         message = request.data["message"]
 
-        self.send_sms(to=to, message=message)
+        message = self._send_message(to, message, Message.SMS)
+        data, status_code = self._handle_response(message)
 
-        return DRFResponse(request.data, status=status.HTTP_200_OK)
+        return DRFResponse(data, status=status_code)
