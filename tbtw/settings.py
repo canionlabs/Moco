8cdd6aefb4755ed043227368204c768058ba739d
caiovictormc <caiovictormc@gmail.com>
2020-03-29 20:28:26 -0300

    Setup static settings

--- tbtw/settings.py
+++ tbtw/settings.py
@@ -139,4 +139,7 @@ REST_FRAMEWORK = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
