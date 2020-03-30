52c6b356b8ecf0f16cc68bcd7358b64b4fe3af54
caiovictormc <caiovictormc@gmail.com>
2020-03-29 22:56:31 -0300

    Register MessageAdmin

--- messages_tw/admin.py
+++ messages_tw/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Message
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = [
+        "pk", "creator", "number_from", "number_to", "kind",
+        "created_at", "has_blocked", "has_error",
+    ]
+    list_filter = ["kind", "created_at", "has_blocked", "has_error"]
+    search_fields = ["number_from", "number_to", "content"]
