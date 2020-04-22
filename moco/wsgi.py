8f10b8a116e95894c0a68489f7fc22136f2a4cc2
Victor Fontenele <victorvitoria11@hotmail.com>
2020-04-22 16:47:15 -0300

    fix: renamed project

--- moco/wsgi.py
+++ moco/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for tbtw project.
+WSGI config for moco project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tbtw.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moco.settings')
 
 application = get_wsgi_application()
